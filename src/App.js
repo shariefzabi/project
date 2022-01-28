33f13269c40bd90bb99dfc15a0a6da357cbccecc
shiv dasari <shiv.dasari@thomsonreuters.com>
2022-01-28 18:02:20 +0530

    base components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './modules/home/Home';
+import Dashboard from './modules/dashboard/dashboard';
 
 function App() {
   return (
     <div className="App">
+      <Home />
+      <Dashboard />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/modules/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import './dashboard.css';
+
+
+function Dashboard(){
+    return (<div className='dashboard'>
+        <h1>Dashboard component</h1>
+    </div>);
+}
+
+export default Dashboard;
