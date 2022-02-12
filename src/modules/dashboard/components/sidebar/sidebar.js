915f50a36abba65e6b8c48d802a3025b38d769d4
Yash1th <yashwanthmukka2000@gmail.com>
2022-02-12 20:44:39 +0530

    Update Sidebar.js

--- src/modules/dashboard/components/sidebar/sidebar.js
+++ src/modules/dashboard/components/sidebar/sidebar.js
@@ -14,13 +14,13 @@ export default function Sidebar() {
 
     return (
         <div>
-            <nav class="sidebar position-fixed d-none d-sm-block">
-                <ul class="nav flex-column fixed-left">
-                    <li class="nav-item">
-                        <a class="btn nav-link text-secondary active" tabindex="0" role="button" data-mdb-toggle="popover"
+            <nav className="sidebar position-fixed d-none d-sm-block">
+                <ul className="nav flex-column fixed-left">
+                    <li className="nav-item">
+                        <a className="btn nav-link text-secondary active" tabindex="0" role="button" data-mdb-toggle="popover"
                             data-mdb-trigger="focus" data-mdb-content="Overview" href="overview.html">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-house" viewBox="0 0 16 16">
+                                className="bi bi-house" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
                                     d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
                                 <path fill-rule="evenodd"
@@ -28,28 +28,28 @@ export default function Sidebar() {
                             </svg>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-secondary" href="profile.html">
+                    <li className="nav-item">
+                        <a className="nav-link text-secondary" href="profile.html">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-person" viewBox="0 0 16 16">
+                                className="bi bi-person" viewBox="0 0 16 16">
                                 <path
                                     d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                             </svg>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-secondary" href="orders.html">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag"
+                    <li className="nav-item">
+                        <a className="nav-link text-secondary" href="orders.html">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bag"
                                 viewBox="0 0 16 16">
                                 <path
                                     d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
                             </svg>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-secondary" href="invoice.html">
+                    <li className="nav-item">
+                        <a className="nav-link text-secondary" href="invoice.html">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-receipt" viewBox="0 0 16 16">
+                                className="bi bi-receipt" viewBox="0 0 16 16">
                                 <path
                                     d="M1.92.506a.5.5 0 0 1 .434.14L3 1.293l.646-.647a.5.5 0 0 1 .708 0L5 1.293l.646-.647a.5.5 0 0 1 .708 0L7 1.293l.646-.647a.5.5 0 0 1 .708 0L9 1.293l.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .708 0l.646.647.646-.647a.5.5 0 0 1 .801.13l.5 1A.5.5 0 0 1 15 2v12a.5.5 0 0 1-.053.224l-.5 1a.5.5 0 0 1-.8.13L13 14.707l-.646.647a.5.5 0 0 1-.708 0L11 14.707l-.646.647a.5.5 0 0 1-.708 0L9 14.707l-.646.647a.5.5 0 0 1-.708 0L7 14.707l-.646.647a.5.5 0 0 1-.708 0L5 14.707l-.646.647a.5.5 0 0 1-.708 0L3 14.707l-.646.647a.5.5 0 0 1-.801-.13l-.5-1A.5.5 0 0 1 1 14V2a.5.5 0 0 1 .053-.224l.5-1a.5.5 0 0 1 .367-.27zm.217 1.338L2 2.118v11.764l.137.274.51-.51a.5.5 0 0 1 .707 0l.646.647.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.646.646.646-.646a.5.5 0 0 1 .708 0l.509.509.137-.274V2.118l-.137-.274-.51.51a.5.5 0 0 1-.707 0L12 1.707l-.646.647a.5.5 0 0 1-.708 0L10 1.707l-.646.647a.5.5 0 0 1-.708 0L8 1.707l-.646.647a.5.5 0 0 1-.708 0L6 1.707l-.646.647a.5.5 0 0 1-.708 0L4 1.707l-.646.647a.5.5 0 0 1-.708 0l-.509-.51z" />
                                 <path
@@ -57,10 +57,10 @@ export default function Sidebar() {
                             </svg>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-secondary" href="payments.html">
+                    <li className="nav-item">
+                        <a className="nav-link text-secondary" href="payments.html">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-credit-card-2-front" viewBox="0 0 16 16">
+                                className="bi bi-credit-card-2-front" viewBox="0 0 16 16">
                                 <path
                                     d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h12zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2z" />
                                 <path
@@ -68,10 +68,10 @@ export default function Sidebar() {
                             </svg>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-secondary" href="trackOrder.html">
+                    <li className="nav-item">
+                        <a className="nav-link text-secondary" href="trackOrder.html">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-mailbox" viewBox="0 0 16 16">
+                                className="bi bi-mailbox" viewBox="0 0 16 16">
                                 <path
                                     d="M4 4a3 3 0 0 0-3 3v6h6V7a3 3 0 0 0-3-3zm0-1h8a4 4 0 0 1 4 4v6a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V7a4 4 0 0 1 4-4zm2.646 1A3.99 3.99 0 0 1 8 7v6h7V7a3 3 0 0 0-3-3H6.646z" />
                                 <path
@@ -79,10 +79,10 @@ export default function Sidebar() {
                             </svg>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-secondary" href="logOut.html">
+                    <li className="nav-item">
+                        <a className="nav-link text-secondary" href="logOut.html">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                                className="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
                                     d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z" />
                                 <path fill-rule="evenodd"
@@ -94,20 +94,20 @@ export default function Sidebar() {
             </nav>
 
             <nav>
-                <div class="d-sm-none position-fixed">
-                    <a class="btn-togglemenu" type="button" onClick={MenuToggler}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list"
+                <div className="d-sm-none position-fixed">
+                    <a className="btn-togglemenu" type="button" onClick={MenuToggler}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list"
                             viewBox="0 0 16 16">
                             <path fill-rule="evenodd"
                                 d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                         </svg>
                     </a>
                 </div>
-                <section id="mobile-menu" class="mobile-menu">
-                    <div class="container">
-                        <a type="button" class="btn-closemenu" onClick={CloseMenu}>
+                <section id="mobile-menu" className="mobile-menu">
+                    <div className="container">
+                        <a type="button" className="btn-closemenu" onClick={CloseMenu}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                class="bi bi-x-lg" viewBox="0 0 16 16">
+                                className="bi bi-x-lg" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd"
                                     d="M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z" />
                                 <path fill-rule="evenodd"
@@ -116,7 +116,7 @@ export default function Sidebar() {
                         </a>
                     </div>
                     <div>
-                        <ul class="menupage">
+                        <ul className="menupage">
                             <li><a>Overview</a></li>
                             <li><a>Orders</a></li>
                             <li><a>Invoice</a></li>
@@ -126,10 +126,10 @@ export default function Sidebar() {
                             <li><a>Notifications</a></li>
                         </ul >
                     </div >
-                    <footer class="terms">
-                        <a class="text-light text-decoration-none" href="#">FAQ</a>
-                        <a class="text-light text-decoration-none" href="#">PRIVACY</a>
-                        <a class="text-light text-decoration-none" href="#">TERMS & CONDITIONS</a>
+                    <footer className="terms">
+                        <a className="text-light text-decoration-none" href="#">FAQ</a>
+                        <a className="text-light text-decoration-none" href="#">PRIVACY</a>
+                        <a className="text-light text-decoration-none" href="#">TERMS & CONDITIONS</a>
                     </footer>
                 </section >
             </nav >
