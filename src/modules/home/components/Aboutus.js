60c25c9d74b6aa08695acb4685a6e7a4e3b41920
CHINTALAPUDI LAKSHMI <srchinni68897@gmail.com>
2022-02-14 07:48:50 -0800

    aboutus and aboutus page css in react

---
+++ src/modules/home/components/Aboutus.js
@@ -0,0 +1,9 @@
+import "./assets/Aboutus.css"
+export default function Aboutus() {
+    return (
+        <section className="aboutus">
+            <h1>About Us</h1>
+            <p className="aboutus-content">Home/About Us</p>
+        </section>
+    )
+}
