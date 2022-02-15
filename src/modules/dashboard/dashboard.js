119d62bd9332ca4e5ba7d3e2470aef76c3f21df0
Spandana G N <spandana3557@gmail.com>
2022-02-15 17:27:22 +0530

    modified logout page

--- src/modules/dashboard/dashboard.js
+++ src/modules/dashboard/dashboard.js
@@ -5,21 +5,23 @@ import Header from './components/header/Header';
 import Sidebar from './components/sidebar/sidebar';
 import Footer from './components/footer/footer';
 import Overview from './components/overview/overview';
-// import LogOut from './components/logOut/logOut';
-// import Statistics from './components/overview/statisticsSection/statistics';
+import LogOut from './components/logOut/logOut';
+
+
 
 
 
 
 function Dashboard() {
-    return (<div className='dashboard'>
+    return (
+      <div className="dashboard">
         <Header></Header>
         <Sidebar></Sidebar>
         <Overview></Overview>
-        {/* <Statistics></Statistics> */}
+        <LogOut></LogOut>
         <Footer></Footer>
-        {/* <LogOut></LogOut> */}
-    </div>);
+      </div>
+    );
 }
 
 export default Dashboard;
