7edb557a385620c013e127366b0438d45137e829
mounikamorampudi <mounikamorampudi777@gmail.com>
2022-02-15 18:42:11 +0530

    invoicepart

--- src/modules/dashboard/dashboard.js
+++ src/modules/dashboard/dashboard.js
@@ -4,8 +4,9 @@ import './dashboard.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/sidebar';
 import Footer from './components/footer/footer';
-import Overview from './components/overview/overview';
-import LogOut from './components/logOut/logOut';
+// import Overview from './components/overview/overview';
+// import LogOut from './components/logOut/logOut';
+import Invoice from './components/invoice/invoice';
 
 
 
@@ -17,8 +18,9 @@ function Dashboard() {
       <div className="dashboard">
         <Header></Header>
         <Sidebar></Sidebar>
-        <Overview></Overview>
-        <LogOut></LogOut>
+        {/* <Overview></Overview> */}
+        <Invoice></Invoice>
+        {/* <LogOut></LogOut> */}
         <Footer></Footer>
       </div>
     );
