3033bd85d187e306b16dd854815e90609267f47f
yallamaraju-charan-varma <95687280+yallamaraju-charan-varma@users.noreply.github.com>
2022-02-15 19:01:27 +0530

    responsive mod

--- src/modules/home/components/blogs/blogs.js
+++ src/modules/home/components/blogs/blogs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import arrow from "./assets/img/Icon (1).png";
+import "./assets/styles.css";
 
 class Blogtable extends React.Component {
     render() {
@@ -9,18 +10,21 @@ class Blogtable extends React.Component {
                     <h1 className="text-center">Blog</h1>
                     <p className="aboutus-content text-center">Home/Blog</p>
                 </section>
+                <section class="blog-content">
+                    <article class="text-center txt">The latest and best articles selected by our editorial choice</article>
+                </section>
                 <section className="text-center">
-                    <div className="d-flex justify-content-between main_box ">
+                    <div className="d-lg-flex justify-content-between main_box ">
                         <div className="left_box">
                             <div className="first_box md-2"><p className="info">ONLINE PLATFORM TO BRIDGE IN LIVESTOCK.....</p></div>
-                            <div className="d-flex justify-content-between two_box">
+                            <div className="d-lg-flex justify-content-between two_box">
                                 <div className="second_box"><p className="info_ver">WHY DO ROASTERS
                                     CROW ALL DAY?</p> </div>
                                 <div className="third_box "><p className="info_ver">THE MERIT OF RUNNING
                                     AN AGRICU-LTURAL BU.....</p></div>
                             </div>
-                            <div className="img_hor"><p className="info">ONLINE PLATFORM TO BRIDGE IN LIVESTOCK.....</p></div>
-                            <div className="d-flex justify-content-between two_box">
+                            <div className="img_hor"><p className="info">ONLINE PLATFORM TO BRIDGE IN LIVESTOCK.....</p><img className="arrow_img" src={arrow} /></div>
+                            <div className="d-lg-flex justify-content-between two_box">
                                 <div className="second_box"><p className="info_ver">COW MILK
                                     SHOULDN’T BE CONSUM....</p> </div>
                                 <div className="third_box"><p className="info_ver">THE MERIT OF RUNNING
@@ -28,14 +32,14 @@ class Blogtable extends React.Component {
                             </div>
                         </div>
                         <div className="right_box">
-                            <div className="d-flex justify-content-between two_box">
+                            <div className="d-lg-flex justify-content-between two_box">
                                 <div className="second_box "><p className="info_ver">COW MILK
                                     SHOULDN’T BE CONSUM....</p> </div>
                                 <div className="third_box img_box"><p className="info_ver">THE MERIT OF RUNNING
                                     AN AGRICU-LTURAL BU.....</p><img className="arrow_img" src={arrow} /></div>
                             </div>
                             <div className="first_box md-2"><p className="info">ONLINE PLATFORM TO BRIDGE IN LIVESTOCK.....</p></div>
-                            <div className="d-flex justify-content-between two_box">
+                            <div className="d-lg-flex justify-content-between two_box">
                                 <div className="second_box"><p className="info_ver">COW MILK
                                     SHOULDN’T BE CONSUM....</p> </div>
                                 <div className="third_box"><p className="info_ver">THE MERIT OF RUNNING
