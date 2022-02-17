b4c671d91a319705a281fb268a62a6bfea887633
yagallasravya <yagallasravya@gmail.com>
2022-02-17 05:41:50 -0800

    overviewOrderspart

--- src/modules/dashboard/components/overview/overview.js
+++ src/modules/dashboard/components/overview/overview.js
@@ -5,6 +5,7 @@ import Quicklinks from './components/quicklinks/quicklinks';
 import TrackOrder from './components/trackOrderSection/trackOrder';
 import Statistics from './components/statisticsSection/statistics';
 import Invoice from './components/invoiceSection/invoice';
+import Orders from './components/ordersSection/orders';
 
 
 
@@ -12,7 +13,7 @@ import Invoice from './components/invoiceSection/invoice';
 function Overview() {
     return (<div className='dashboard'>
         <Quicklinks></Quicklinks>
-
+        <Orders></Orders>
         <Invoice></Invoice>
         <TrackOrder></TrackOrder>
         <Statistics></Statistics>
--- src/modules/dashboard/dashboard.js
+++ src/modules/dashboard/dashboard.js
@@ -3,11 +3,11 @@ import './dashboard.css';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/sidebar';
 import Footer from './components/footer/footer';
-import Orders from './components/orders/orders';
-//import Overview from './components/overview/overview';
+//import Orders from './components/orders/orders';
+import Overview from './components/overview/overview';
 // import LogOut from './components/logOut/logOut';
 // import TrackOrder from './components/trackOrder/trackOrder';
-// import Invoice from './components/invoice/invoice';
+//import Invoice from './components/invoice/invoice';
 // import Payments from './components/payments/payments';
 
 
@@ -20,12 +20,12 @@ function Dashboard() {
       <div className="dashboard">
         <Header></Header>
         <Sidebar></Sidebar>
-        {/* <Overview></Overview>  */}
+        <Overview></Overview> 
         {/* <TrackOrder></TrackOrder>  */}
-          {/* <Invoice></Invoice> */}
+        {/* <Orders></Orders> */}
+        {/* <Invoice></Invoice> */}
           {/* <LogOut></LogOut>         */}
          {/* <Payments></Payments> */}
-         <Orders></Orders>
        <Footer></Footer>
         
       </div>
