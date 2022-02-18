86c3a03847e448293d6455131ea6600801bdd5c7
UHA LAKSHMI TATIKONDA <uhalakshmit@gmail.com>
2022-02-18 18:32:50 +0530

    changes in blogcontent page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 import './App.css';
 // import Home from './modules/home/Home';
 // import Dashboard from './modules/dashboard/dashboard';
-// import Finalblog from './modules/home/components/Blogcontent/final';
+import Finalblog from './modules/home/components/Blogcontent/final';
 // import LandingPage from './modules/home/components/LandingPage';
 // import "./modules/home/components/blogs/assets/styles.css";
 // import Final from "./modules/home/components/blogs/final";
 // import Aboutus from "./modules/home/components/AboutusPage(MOD)/Aboutus"
 // import ButcheryPopup from "./modules/home/components/butchery_form/butcherypopup"
-import BeanAgentPopup from "./modules/home/components/be an agent form/form1"
+// import BeanAgentPopup from "./modules/home/components/be an agent form/form1"
 // import Butcherypopup from "./modules/home/components/form2/form2"
 // import AgentModel from "./modules/home/components/AgentModel/AgentModel1"
 
@@ -19,12 +19,12 @@ function App() {
       {/* <LandingPage/> */}
     {/* <Dashboard/> */}
       {/* <LandingPage></LandingPage> */}
-      {/* <Finalblog/> */}
+      <Finalblog/>
       {/* <Aboutus></Aboutus> */}
       {/* <LandingPage></LandingPage> */}
       {/* <Final></Final> */}
       {/* <ButcheryPopup></ButcheryPopup> */}
-      <BeanAgentPopup></BeanAgentPopup>
+      {/* <BeanAgentPopup></BeanAgentPopup> */}
       {/* <Butcherypopup></Butcherypopup> */}
       {/* <AgentModel></AgentModel> */}
       
--- src/modules/home/components/Blogcontent/final.js
+++ src/modules/home/components/Blogcontent/final.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./assets/index.css";
+import "./assets/styles.css";
 import Image from "./image";
 import Middle from "./middle";
 import Form from "./form";
