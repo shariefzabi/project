98fecb6e764ea7e8cc435f0a93c1f9494b527b83
Thota Sridurga Siva Naga Jyothi <64146793+jyothithota19@users.noreply.github.com>
2022-02-26 14:16:49 +0530

    modified index.js

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 let express = require('express');
 let app = express();
-app.use(express.json())
-
 
 app.get("/", function (req, res) {
     let { body, params, query } = req;
     console.log("path:", req.url)
     console.log("params:", params);
-    console.log("params:", body);
-    console.log("params:", query);
-    res.send("Welcome to my home page")
+    console.log("body:", body);
+    console.log("query:", query);
+    res.send("Welcome to Home page")
 })
 
 
-
-const PORT = process.env.PORT || 5005;
+const PORT = 5005;
 app.listen(PORT, function (err, res) {
     if (err) throw err;
     console.log(`Application is started successfully and running on : ${PORT}...`);
