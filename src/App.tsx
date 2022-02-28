fd0600d0d9d0aaea771af1610ef4777119eeacbf
Hareesh yadla <yadlaharish111@gmail.com>
2022-02-28 16:58:41 +0530

    Updating App level Router

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import './App.scss';
 import Dashboard from './modules/dashboard/dashboard';
 import Home from './modules/home/Home';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
+          <Routes>
+            <Route path="/" element={<Home />}>
+              
             </Route>
-            <Route path="/dashboard">
-              <Dashboard />
+            <Route path="/dashboard" element={<Dashboard />}>
+              
             </Route>
-          </Switch>
+            </Routes>
         </div>
       </Router>
     </div>
