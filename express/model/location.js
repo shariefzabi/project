d72fe82c5d32f381b6b7a9dfbdee7b66fa3cf873
yagallasravya <yagallasravya@gmail.com>
2022-03-01 01:11:43 -0800

    validation

--- express/model/location.js
+++ express/model/location.js
@@ -1,7 +1,9 @@
 const mongoose=require("mongoose");
 const Location=new mongoose.Schema({
-    locationName:{type:String}
+    locationName:{type:String,
+        required:true,
+        unique:true   
 }
     
-)
+})
 module.exports =mongoose.model("locationName",Location) ;
