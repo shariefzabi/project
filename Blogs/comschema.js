730536768c726fdb45d766f3effaa449e05abc7c
UHA LAKSHMI TATIKONDA <uhalakshmit@gmail.com>
2022-03-03 14:28:32 +0530

    schemas of comment and blogs

---
+++ Blogs/comschema.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Comments = new mongoose.Schema({
+
+
+
+    comment: {
+        name: { type: String,required:true},
+        email: { type: String,required:true},
+        blog_id:{type:Number,required:true},
+        date: { type: Date, default:Date.now},
+    }
+}
+
+
+);
+module.exports = mongoose.model("comments",Comments);
