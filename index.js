9c706aab72ed6e60e3f4e27d5fb37a5a3caffabd
mounikamorampudi <mounikamorampudi777@gmail.com>
2022-03-04 11:40:13 +0530

    cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors=require("cors");
 const app = express();
+app.use(cors());
 
 const PORT = 3005;
 var mongoClient = require("mongodb").MongoClient;
