e1792ab03f939d1432792e664e3be044640575ea
Spandana G N <spandana3557@gmail.com>
2022-03-04 13:08:19 +0530

    modified cart details

--- cart/cart_details.js
+++ cart/cart_details.js
@@ -1,36 +1,102 @@
 const express = require("express");
-const Animal = require("../locationPoductDetails/marketingDetailsSchema.js");
+const Animal = require("../locationPoductDetails/marketingDetailsSchema");
+const expressAsyncHandler = require("express-async-handler");
+
 const animalAPI = express();
 
-animalAPI.get("/get-animal/:animalID", function (req, res) {
-  const locationdetails = req.app.get("locationdetails");
-  locationdetails.findOne({}, function (err, result) {
-    if (err) {
-      console.log(err);
+animalAPI.get(
+  "/get-animal/:locationName/:animalId",
+  expressAsyncHandler(async (req, res) => {
+    let locationName = req.params.locationName;
+    let animalId = req.params.animalId;
+    //   console.log(animalId);
+    let locationdbs = await Animal.findOne({ locationName: locationName });
+    //   console.log(locationdbs);
+    if (locationdbs == null) {
+      res
+        .status(200)
+        .send({ message: "location does not exist with this location name" });
     } else {
-      if ("locationDetails" in result) {
-        console.log("Animal details are available and can be fetched!");
-
+      // data = {
+      //   price: locationdbs.cattleMarket.price,
+      //   //   type: locationdetails.cattleMarket.type,
+      //   // quantity: locationdetails.quantity,
+      //   //   animalId: locationdetails.cattleMarket.animalId,
+      // };
+      //   for (let animalData of locationdbs.cattleMarkets) {
+      //     if (animalData.animalId == animalId) {
+      //       data = {
+      //         price: animalData.price,
+      //       };
+      //       return res.status(200).send({
+      //         message: "Animal data fetched successfully!!",
+      //         payload: data,
+      //       });
+      //     } else {
+      //       res.status(200).send({
+      //         message: "Animal data doesn't exist with this animal Id!!",
+      //       });
+      //     }
+      //   }
+      let animalData = locationdbs.cattleMarkets.find(
+        (animal) => animal.animalId == animalId
+      );
+      if (animalData == null) {
+        res.status(200).send({
+          message: "Animal data doesn't exist with this animal Id!!",
+        });
+      } else {
         data = {
-          price: result.locationDetails.cattleMarket.price,
-          breed: result.locationDetails.cattleMarket.breed,
-          animalId: result.locationDetails.cattleMarket.animalId,
-          weight: result.locationDetails.cattleMarket.weight,
+          price: animalData.price,
+          animalId: animalData.animalId,
         };
-        console.log(data);
         res.status(200).send({
           message: "Animal data fetched successfully!!",
           payload: data,
         });
-      } else {
-        res
-          .status(200)
-          .send({ message: "Animal does not exists with this animal ID!" });
-        // console.log("Animal does not exists with this animal ID!");
+        console.log(animalData);
       }
     }
-  });
-});
+  })
+);
+
+// animalAPI.get("/get-animal/:locationName", function (req, res) {
+//   let locationName = req.params.locationName;
+//   console.log(locationName);
+//   //   let animalId = req.params.animalId;
+//   //   const locationdbs = req.app.get("locationdbs");
+//   //   console.log(locationdbs);
+//   Animal.findOne({ locationName: locationName }),
+//     function (err, result) {
+//       if (err) {
+//         console.log(err);
+//       } else {
+//         console.log(result);
+//         if ("cattleMarkets" in result) {
+//           console.log("Animal details are available and can be fetched!");
+//           console.log(result);
+//           //   console.log(locationdbs);
+
+//           data = {
+//             price: result.cattleMarket.price,
+//             //   breed: result.locationDetails.cattleMarket.breed,
+//             //   animalId: result.locationDetails.cattleMarket.animalId,
+//             //   weight: result.locationDetails.cattleMarket.weight,
+//           };
+//           console.log(data);
+//           res.status(200).send({
+//             message: "Animal data fetched successfully!!",
+//             payload: data,
+//           });
+//         } else {
+//           res
+//             .status(200)
+//             .send({ message: "Animal does not exists with this animal ID!" });
+//           // console.log("Animal does not exists with this animal ID!");
+//         }
+//       }
+//     };
+// });
 animalAPI.use((err, req, res, next) => {
   res.send({ message: err.message });
 });
