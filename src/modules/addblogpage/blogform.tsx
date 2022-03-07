dca78f4f698b507234e202321eb27c6afe664950
yallamaraju-charan-varma <95687280+yallamaraju-charan-varma@users.noreply.github.com>
2022-03-07 11:15:11 +0530

    add blogs mod

--- src/modules/addblogpage/blogform.tsx
+++ src/modules/addblogpage/blogform.tsx
@@ -1,106 +1,112 @@
 import "./style.css";
 import React from "react";
-import CkEditorExampleComponent from "./CkEditorComponent";
-import Header from "../../components/header/app_header";
-import Footer from "../../components/footer/footer";
+import { Editor } from 'primereact/editor';
+import 'primereact/resources/primereact.min.css'
 
-class  Blogform extends React.Component<any,any>{
-    constructor(props:any) {
+class Blogform extends React.Component<any, any>{
+    constructor(props: any) {
         super(props);
         this.state = {
             topic: "",
-            discripton:"",
-            // Error Messages 
+            text1: '',
+            // Error Messages
+            dicErrMsg: "",
             topicErrMsg: "",
-            topicFlag:false
+            disFlag: false,
+            topicFlag: false
         }
     }
-    getdata(a:any){
-        this.setState({discripton:a})
+    getdata(a: any) {
+        this.setState({ discripton: a })
     }
     changeHandler = (e: any) => {
         this.setState({ [e.target.name]: e.target.value })
     }
     submitHandler = (e: any) => {
-        e.preventDefault();
-        if(this.state.topicFlag){
+        if (this.state.topicFlag && this.state.disFlag) {
             console.log(this.state);
+        }else{
+            this.validate();
         }
     }
-    
+
     validations = (e: any) => {
-        if (e.target.name=== 'topic') {
-            if (this.state.topic == undefined || this.state.topic.length === 0) {
-                this.setState({topicErrMsg:"Please enter the title."})
+        if (this.state.topic == undefined || this.state.topic.length === 0) {
+            this.setState({ topicErrMsg: "Please enter the title." })
+            e.target.classList.add("field-error")
+        } else {
+            let nameReg = /^([a-zA-Z ]{10,100})$/
+            if (!nameReg.test(this.state.topic)) {
+                this.setState({ topicErrMsg: "Accepts Alphabets, space & Min 10 to Max 100 Char" })
                 e.target.classList.add("field-error")
             } else {
-                let nameReg = /^([a-zA-Z ]{10,100})$/
-                // console.log(nameReg.test(username));
-                if (!nameReg.test(this.state.topic)) {
-                    this.setState({ topicErrMsg:"Accepts Alphabets, space & Min 10 to Max 100 Char" })
-                    e.target.classList.add("field-error")
-                } else {
-                    e.target.classList.remove("field-error")
-                    this.setState({topicErrMsg:"",topicFlag:true})
-                }
+                e.target.classList.remove("field-error")
+                this.setState({ topicErrMsg: "", topicFlag: true })
             }
         }
     }
-    reset(){
-        this.setState({topic:"", discripton:"",})
+    validate = () => {
+        if (this.state.topic == "" || this.state.text1.length === 0) {
+            this.setState({ dicErrMsg: "Please enter the Description." })
+        } else {
+            this.setState({ dicErrMsg: "", disFlag: true })
+        }
     }
-    render(){
-        let{topic,topicErrMsg}=this.state;
+   
+
+    render() {
+        let { topic, topicErrMsg } = this.state;
         return (
             <>
-            {/* <header><Header></Header></header> */}
-            <div className="main">
-                <section className="aboutus">
-                    <h1>ADD BLOG</h1>
-                    
-                </section>
+                <div className="main">
+                    <section className="aboutus">
+                        <h1>ADD BLOG</h1>
 
-                <div className="container">
-                    <h2>ADD BLOG</h2>
-                
-                <div  className="inner">
-                    <div className="d-flex box">
-                    <p>Fields with <span className="text-danger">*</span> are required</p>
-                        <button type="button" className="btn btn-success btnreset" onClick={this.reset} >
-                            Reset
-                        </button>
-                    </div>
-                    
-                    <div className="text-start box">
-                        <label className="me-2"><b>Title<span className="text-danger">*</span>:</b></label>
-                        <input placeholder="Title" className=" form-control" name="topic" value={topic}  onChange={(e)=>{this.changeHandler(e);this.validations(e)}}></input>
-                        <div>
-                            <p className="text-danger">{topicErrMsg}</p>
+                    </section>
+
+                    <div className="container">
+                        <h2>ADD BLOG</h2>
+
+                        <div className="inner">
+                            <div className=" box">
+                                <p>Fields with <span className="text-danger">*</span> are required</p>
+                            </div>
+
+                            <div className="text-start box">
+                                <label className="me-2"><b>Title<span className="text-danger">*</span>:</b></label>
+                                <input placeholder="Title" className=" form-control" name="topic" value={topic} onChange={(e) => { this.changeHandler(e); this.validations(e) }}></input>
+                                <div>
+                                    <p className="text-danger">{topicErrMsg}</p>
+                                </div>
+                            </div>
+                            <div className="box">
+                                <div className="mb-3">
+                                    <label className="me-2"><b>Image<span className="text-danger">*</span>:</b></label>
+                                    <input type="file" className="form-control" aria-label="file example" required />
+                                        <div className="invalid-feedback">Example invalid form file feedback</div>
+                                </div>
+                            </div>
+                            <div className="box">
+                                <label className="me-3 mt-3"><b>Description<span className="text-danger">*</span></b></label>
+                                <Editor style={{ height: '320px' }} value={this.state.text1} onTextChange={(e) => {this.setState({ text1: e.htmlValue }) }} />
+                                {!this.state.disFlag &&
+                                    <p className="text-danger">{this.state.disErrMsg}</p>
+                                }
+                            </div>
+                            <div className="mt-3 text-center">
+                                <button type="submit" className="btn form_button btn-success " onClick={this.submitHandler}>
+                                    Save
+                                </button>
+                            </div>
                         </div>
                     </div>
-                    <div className="box">
-                        <label className="me-3 mt-3"><b>Upload Image<span className="text-danger">*</span></b></label>
-                        <input type="file" id="myFile" name="filename"></input>
-                    </div>
-                    <div className="box">
-                        <label className="me-3 mt-3"><b>discripton<span className="text-danger">*</span>:</b></label>
-                        <CkEditorExampleComponent ></CkEditorExampleComponent>
-                    </div>
-                    <div className="mt-3 text-center">
-                        <button type="button" className="btn form_button btn-success " onClick={this.submitHandler}>
-                            Save
-                        </button>
-                    </div>
-                    </div>
+
                 </div>
-    
-            </div>
-            {/* <footer><Footer></Footer></footer> */}
             </>
-    
+
         );
     }
-    
+
 }
 
 export default Blogform;
