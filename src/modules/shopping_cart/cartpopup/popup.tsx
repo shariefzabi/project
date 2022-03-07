e74da6b9b84d79450cf48dbdf00fae49647c22f4
priankabaptist <95601571+priankabaptist@users.noreply.github.com>
2022-03-07 09:40:05 +0530

    modified

--- src/modules/shopping_cart/cartpopup/popup.tsx
+++ src/modules/shopping_cart/cartpopup/popup.tsx
@@ -9,7 +9,7 @@ export default class Header extends React.Component {
                     <div className="d-flex flex-row-reverse bd-highlight">
                         <nav className="navbar navbar-expand-lg navbar-light">
                             <div className="container-fluid">
-                                <button className="navbar-toggler" type="button " data-bs-toggle="collapse"
+                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                                     aria-label="Toggle navigation">
                                     <span className="navbar-toggler-icon "></span>
