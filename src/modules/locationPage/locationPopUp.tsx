1d2d5bf2ff27446d9d21b59d958c592aafae460b
yagallasravya <yagallasravya@gmail.com>
2022-03-08 01:02:37 -0800

    fe validations

--- src/modules/locationPage/locationPopUp.tsx
+++ src/modules/locationPage/locationPopUp.tsx
@@ -11,7 +11,7 @@ function PopUp(prop: any) {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  let [locationNames, setLocationNames] = useState([])
+  let [locationNames, setLocationNames] = useState<string[]>([])
   let [isAdding, setIsAdding] = useState(false)
   let [newLocation, setNewLocation] = useState('')
   let [newLocationErr, setNewLocationErr] = useState('')
@@ -48,8 +48,17 @@ function PopUp(prop: any) {
           setNewLocationErr(fullNameErrMsg)
           e.target.classList.add("field-error")
         } else {
-          e.target.classList.remove("field-error")
-          setNewLocationErr("")
+          let valid = locationNames.includes(enteredLocation)
+          console.log(valid)
+          if(valid){
+            let fullNameErrMsg = "already exists in list"
+            setNewLocationErr(fullNameErrMsg)
+            e.target.classList.add("field-error")
+          }else{
+            e.target.classList.remove("field-error")
+            setNewLocationErr("")
+          }
+         
         }
       }
 
@@ -58,14 +67,14 @@ function PopUp(prop: any) {
   }
   const LocationSubmitHandler = (e: any, newLocation: any) => {
     e.preventDefault();
-
+     
 
     if (newLocationErr == "") {
       addNewLocation()
       setIsAdding(false);
     }
 
-
+   setNewLocation("")
     console.log(newLocation)
   }
 
@@ -136,7 +145,7 @@ function PopUp(prop: any) {
                   <button className="btn-success btn-add ms-5" type="button" onClick={startAddingHandler}>Add&#32;Location</button>
                 </div>}
 
-                {isAdding && <form className="mt-3 needs-validation ms-5"
+                {isAdding && <form className="d-inline mt-3 needs-validation ms-5"
                   onSubmit={(e) => LocationSubmitHandler(e, { newLocation })} >
                   <label htmlFor="ulocation" className="form-label"></label>
                   <input className="addLocation "
@@ -149,11 +158,12 @@ function PopUp(prop: any) {
                     onChange={(e) => setNewLocation(e.target.value)}
 
                     required />
-                  <p className="text-danger">{newLocationErr}</p>
+                 
 
-                  <button className="btn-success btn-add" type="submit" >
+                  <button className="d-inline btn-success btn-add" type="submit" >
                     Save&#32;Location
                   </button>
+                  <p className="text-danger ms-5 mb-0">{newLocationErr}</p>
                 </form>}
 
                 <form className="needs-validation" >
