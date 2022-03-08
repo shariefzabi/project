6c7d4f991d06f307f7a5619e972848247ac7c136
manjusunkad8899 <manjusunkad8899@gmail.com>
2022-03-08 12:29:02 +0530

    Redux Integration PopUp

--- src/modules/locationPage/locationPopUp.tsx
+++ src/modules/locationPage/locationPopUp.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import Modal from '@mui/material/Modal';
-import { Link } from "react-router-dom";
-import ProductDetails from "./productDetailsPage/ProductDetails"
 import axios from 'axios';
 import { connect } from "react-redux";
 import redux from 'react-redux'
@@ -38,15 +36,15 @@ function PopUp(prop: any) {
         setNewLocationErr("Location field should not be empty")
         e.target.classList.add("field-error")
       }
-      //  else if(locationNames.includes([enteredLocation])) {
+      //  else if(locationNames.find("enteredLocation")) {
       //   setNewLocationErr("goovinda")
       //   e.target.classList.add("field-error")
       // }
       else {
-        let nameReg = /^([a-zA-Z ]{3,20})$/
+        let nameReg = /^([a-zA-Z ]{4,15})$/
 
         if (!nameReg.test(enteredLocation)) {
-          let fullNameErrMsg = "Accepts Alphabets, space & Min 3 to Max 20 Char"
+          let fullNameErrMsg = "Accepts Alphabets, space & Min 3 to Max 15 Char"
           setNewLocationErr(fullNameErrMsg)
           e.target.classList.add("field-error")
         } else {
@@ -122,95 +120,90 @@ function PopUp(prop: any) {
         <div className='location-modal-container'>
           <div id="modal-modal-description" >
 
-    // const stopAddingHandler = () => {
-    //   addNewLocation()
-    //   setIsAdding(false);
-    // }
+
+            <div className="popUp">
+              <main className="popUpContent">
+                <h3 className="locationHeading">
+                  Choose&#32;your&#32;preferred&#32;location
+                </h3>
+                <article className="article">
+                  This&#32;displays&#32;all&#32;products&#32;associated&#32;with&#32;the&#32;chosen&#32;address
+                </article>
 
 
+                {!isAdding && <div className=" addLocbtn mt-3">
+                  <p className=" pLoc d-inline">Can&#39;t&#32;find&#32;your&#32;location&#63;</p>
+                  <button className="btn-success btn-add ms-5" type="button" onClick={startAddingHandler}>Add&#32;Location</button>
+                </div>}
 
-    return (
-            <div className="d-flex">
-              <button className="pt-2 buy_button btn  text-light"
-                onClick={() => { handleOpen() }}>Buy Now</button>
-              <Modal
-                open={open}
-                onClose={handleClose}
-                aria-describedby="modal-modal-description"
-                sx={{ overflow: 'auto' }}
-              >
-                <div className='location-modal-container'>
-                  <div id="modal-modal-description" >
+                {isAdding && <form className="mt-3 needs-validation ms-5"
+                  onSubmit={(e) => LocationSubmitHandler(e, { newLocation })} >
+                  <label htmlFor="ulocation" className="form-label"></label>
+                  <input className="addLocation "
+                    type="text" id="ulocation"
 
+                    placeholder="Enter your Location here"
+                    value={newLocation}
+                    onBlur={(e) => validations(e)}
+                    name="newLocation"
+                    onChange={(e) => setNewLocation(e.target.value)}
 
-                    <div className="popUp">
-                      <main className="popUpContent">
-                        <h3 className="locationHeading">
-                          Choose&#32;your&#32;preferred&#32;location
-                        </h3>
-                        <article className="article">
-                          This&#32;displays&#32;all&#32;products&#32;associated&#32;with&#32;the&#32;chosen&#32;address
-                        </article>
+                    required />
+                  <p className="text-danger">{newLocationErr}</p>
 
+                  <button className="btn-success btn-add" type="submit" >
+                    Save&#32;Location
+                  </button>
+                </form>}
 
-                        {!isAdding && <div className=" addLocbtn mt-3">
-                          <p className=" pLoc d-inline">Can&#39;t&#32;find&#32;your&#32;location&#63;</p>
-                          <button className="btn-success btn-add ms-5" type="button" onClick={startAddingHandler}>Add&#32;Location</button>
-                        </div>}
+                <form className="needs-validation" >
+                  <div className="dropDown " >
+                    <select className="form-select dropdownToggle"
+                      placeholder="Location"
+                      onClick={fetchLocations}
+                      required>
 
-                        {isAdding && <form className="mt-3 needs-validation ms-5"
-                          onSubmit={(e) => LocationSubmitHandler(e, { newLocation })} >
-                          <label htmlFor="ulocation" className="form-label"></label>
-                          <input className="addLocation "
-                            type="text" id="ulocation"
+                      <option className="dropdownItem" selected disabled value="" hidden>Location</option>
+                      {
+                        locationNames.map(location => {
+                          return (
+                            <option className="dropdownItem" >
+                              {location}
+                            </option>
+                          )
+                        })
+                      }
 
-                            placeholder="Enter your Location here"
-                            value={newLocation}
-                            onBlur={(e) => validations(e)}
-                            name="newLocation"
-                            onChange={(e) => setNewLocation(e.target.value)}
+                    </select>
 
-                            required />
-                          <p className="text-danger">{newLocationErr}</p>
-
-                          <button className="btn-success btn-add" type="submit" >
-                            Save&#32;Location
-                          </button>
-                        </form>}
-
-                        <form className="needs-validation" >
-                          <div className="dropDown " >
-                            <select className="form-select dropdownToggle"
-                              placeholder="Location"
-                              onClick={fetchLocations}
-                              required>
-
-                              <option className="dropdownItem" selected disabled value="" hidden>Location</option>
-                              {
-                                locationNames.map(location => {
-                                  return (
-                                    <option className="dropdownItem" >
-                                      {location}
-                                    </option>
-                                  )
-                                })
-                              }
-
-                            </select>
-
-                          </div>
-
-                          {/* <button className="btn-success btn-cnt" type="submit" >Continue</button> */}
-                          <Link to="/ProductDetails" className="btn-success btn-cnt" type="submit" >Continue</Link>
-
-                        </form>
-                      </main>
-                    </div >
                   </div>
 
-                </div>
-              </Modal>
+                  <button className="btn-success btn-cnt" type="submit" onClick={(e) => popUproute(e)}>Continue</button>
 
+                </form>
+              </main>
+            </div >
+          </div>
+        </div>
+      </Modal>
 
-              export default connect(mapStateToProps, mapDispatchToProps)(PopUp);
+    </div>
+
+  )
+}
+const mapStateToProps = (state: any) => {
+  console.log(state);
+
+  return {
+    redux: state
+  }
+}
+
+const mapDispatchToProps = (dispatch: Function) => {
+  return {
+    pop_up_toggle: (toggleflag: any) => dispatch({ type: 'toggle_flag', payload: toggleflag })
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PopUp);
 
