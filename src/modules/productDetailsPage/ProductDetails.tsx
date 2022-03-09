04688ee88713507239cb0638cbea0a80abfe504f
Yash1th <yashwanthmukka2000@gmail.com>
2022-03-09 15:44:22 +0530

    Breadcrumbs in product page

--- src/modules/productDetailsPage/ProductDetails.tsx
+++ src/modules/productDetailsPage/ProductDetails.tsx
@@ -1,39 +1,42 @@
-import React, { useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import './productDetails.css';
 import { connect } from "react-redux";
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Link } from "react-router-dom";
+
 
 
 
 function ProductDetails(props: any) {
 
     let [products, setProducts] = useState([])
-    let [marketType,setMarketType]=useState("")
+    let [marketType, setMarketType] = useState("")
     // console.log(props.locName)
     let selectedLocationName = props.state.locName
 
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get("http://localhost:3005/market/marketDetails")
-        .then(
-            response=>{
-                setProducts(response.data)
-            }
-        )
-    },[]);
+            .then(
+                response => {
+                    setProducts(response.data)
+                }
+            )
+    }, []);
 
-    const marketTypeHandler=()=>{
+    const marketTypeHandler = () => {
         setMarketType("Cattle Market")
     }
-    const markettypeHandler=()=>{
+    const markettypeHandler = () => {
         setMarketType("Sheep Market")
     }
-    
-
-    
 
 
-    
+
+
+
+
     // const fetchProducts = (async () => {
     //     try {
     //         const response = await axios.get("http://localhost:3005/market/marketDetails", { params: { locationName: { selectedLocationName } } })
@@ -49,25 +52,36 @@ function ProductDetails(props: any) {
     return (
         <div id="productPage">
             {/* <p id="sub">Home / Location / Cattle Market</p> */}
+            <Breadcrumb id="sub">
+                <Breadcrumb.Item>
+                    <Link className="breadCrumbs" to='/'>Home</Link>
+                </Breadcrumb.Item>
+                <Breadcrumb.Item>
+                    <Link className="breadCrumbs" to='/location'>{selectedLocationName}</Link>
+                </Breadcrumb.Item >
+                <Breadcrumb.Item active>
+                    selectedMarket
+                </Breadcrumb.Item>
+            </Breadcrumb>
             <div className="container">
                 <div className="row">
                     <div className="col-2 locNames ">
-                        <div>                          
-                           {
-                               products.map((e:any)=>{
-                                   return(
-                                    <div>
-                                    <button className="location-btn btn " role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">                                                                                
-                                        {e.locationName}                                                                              
-                                    </button>
-                                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                        <li><a className="dropdown-item" onClick={marketTypeHandler} >Cattle Market({e.cattleMarkets.length})</a></li>
-                                        <li><a className="dropdown-item" onClick={markettypeHandler}>Sheep Market({e.sheepMarkets.length})</a></li>
-                                    </ul>
-                                    </div>
-                                   )
-                               })                           
-                       }
+                        <div>
+                            {
+                                products.map((e: any) => {
+                                    return (
+                                        <div>
+                                            <button className="location-btn btn " role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                                                {e.locationName}
+                                            </button>
+                                            <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                                <li><a className="dropdown-item" onClick={marketTypeHandler} >Cattle Market({e.cattleMarkets.length})</a></li>
+                                                <li><a className="dropdown-item" onClick={markettypeHandler}>Sheep Market({e.sheepMarkets.length})</a></li>
+                                            </ul>
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
                     </div>
                     <div className="col-10">
@@ -104,7 +118,7 @@ function ProductDetails(props: any) {
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 const mapStateToProps = (state: any) => {
