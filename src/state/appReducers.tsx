0a249d98ce4c51a854cf12afba20a27b53842768
srinivasvicky57 <srinivasvicky57@gmail.com>
2022-03-09 11:02:31 +0530

    restored backup code (team 4)
    No issues with our commit

--- src/state/appReducers.tsx
+++ src/state/appReducers.tsx
@@ -1,48 +1,48 @@
 interface initialState {
-  user: any;
-  products: any[];
-  orders: {};
+  user: any,
+  products: any[],
+  orders: {},
 
-  cardDetails: any;
-  agent: any;
-  Butchery: any;
+  cardDetails:any
+  agent:any
+  Butchery:any
+  locName:any
 }
 
 const appInitialstate: initialState = {
   user: null,
   products: [],
   orders: {},
-  cardDetails: null,
-  agent: null,
-  Butchery: null,
-};
+  cardDetails:null,
+  agent:null,
+  Butchery:null,
+  locName:null
+}
 function appReducer(state = appInitialstate, action: any) {
   switch (action.type) {
-    case "logout": {
-      return { ...state, user: null };
-    }
-    case "setUser":
-      return { ...state, user: action.payload };
-    case "setAgent":
-      return { ...state, agent: action.payload };
-    case "setButchery":
-      return { ...state, agent: action.payload };
+      case 'logout': {
+          return { ...state, user: null };
+      }
+      case 'setUser':
+          return { ...state, user: action.payload };
+      case 'setAgent':
 
-    case "store_products":
-      return { ...state, products: [...state.products, action.productDetails] };
-    case "store_productdetails":
-      return { ...state, orders: { productdetails: action.productDetails } };
-    case "store_order":
-      return {
-        ...state,
-        orders: { ...state.orders, deliverydetails: action.deliveryDetails },
-      };
-    case "storeCardDetails":
-      return { ...state, cardDetails: action.payload };
-    case "toggle_flag":
-      return { ...state, toggleflag: action.payload };
-    default:
-      return state;
+          return {...state, agent: action.payload};
+          case 'setButchery':
+              return {...state, agent: action.payload};
+          
+      case 'store_products' :
+      return { ...state, products: [...state.products, action.productDetails] };   
+      case 'store_productdetails':
+          return { ...state, orders: { productdetails: action.productDetails } }
+      case 'store_order':
+          return { ...state, orders: { ...state.orders, deliverydetails: action.deliveryDetails } }
+      case "storeCardDetails":
+          return ({ ...state, cardDetails: action.payload })
+      case "storeLocname":
+          return ({ ...state, locName: action.payload })
+      default:
+          return state;
   }
 }
 export default appReducer;
