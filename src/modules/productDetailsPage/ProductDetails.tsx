59e8331f287de035fc4442a808e7d1a0b1500043
mounikamorampudi <mounikamorampudi777@gmail.com>
2022-03-09 14:04:41 +0530

    productpage

--- src/modules/productDetailsPage/ProductDetails.tsx
+++ src/modules/productDetailsPage/ProductDetails.tsx
@@ -1,34 +1,38 @@
-import React, { useState } from "react";
+import React, { useState,useEffect} from "react";
 import axios from 'axios';
 import './productDetails.css';
+import { connect } from "react-redux";
 
-  
 
-export default function ProductDetails(props : any) {
+
+function ProductDetails(props: any) {
 
     let [products, setProducts] = useState([])
     // console.log(props.locName)
-    let selectedLocationName=props.locName
+    let selectedLocationName = props.state.locName
 
 
-   
-    const fetchProducts = (async () => {
-        try {
-            const response = await axios.get("http://localhost:3005/market/marketDetails",{params:{locationName:{selectedLocationName}}})
-            setProducts(response.data.args);
-        } catch (err) {
-            console.error(err)
-        }
-    })
-    fetchProducts();
-    console.log(products)
+    useEffect(()=>{
+        axios.get("http://localhost:3005/market/marketDetails")
+        .then(
+            response=>{
+                setProducts(response.data)
+            }
+        )
+    },[]);
 
-
-  
-    
+    // console.log(products)
 
     
-
+    // const fetchProducts = (async () => {
+    //     try {
+    //         const response = await axios.get("http://localhost:3005/market/marketDetails", { params: { locationName: { selectedLocationName } } })
+    //         setProducts(response.data.args);
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
+    // })
+    // fetchProducts();
     // const { locationName } = products
     // console.log(locationName)
 
@@ -38,8 +42,23 @@ export default function ProductDetails(props : any) {
             <div className="container">
                 <div className="row">
                     <div className="col-2 locNames ">
-                        location names
-
+                        <div>                          
+                           {
+                               products.map((e:any)=>{
+                                   return(
+                                    <div>
+                                    <button className="btn " role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">                                                                                
+                                        {e.locationName}                                                                              
+                                    </button>
+                                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                        <li><a className="dropdown-item" >Cattle Market()</a></li>
+                                        <li><a className="dropdown-item">Sheep Market()</a></li>
+                                    </ul>
+                                    </div>
+                                   )
+                               })                           
+                       }
+                        </div>
                     </div>
                     <div className="col-10">
                         <h3 id="marketHeading">Cattle&#32;Market</h3>
@@ -47,25 +66,21 @@ export default function ProductDetails(props : any) {
                         <hr />
                         <div className="card-deck d-flex ">
                             <div className="card col-4 m-2 ">
-                              
                                 <div className="card-body">
                                     <h5 className="card-id">Animal id</h5>
                                     <p className="card-price">Price</p>
                                     <button type="button" className="btn btn-success">Success</button>
-
                                 </div>
                             </div>
                             <div className="card col-4 m-2 ">
-                               
                                 <div className="card-body">
-
                                     <h5 className="card-id">Animal id</h5>
                                     <p className="card-price">Price</p>
                                     <button type="button" className="btn btn-success">Success</button>
                                 </div>
                             </div>
                             <div className="card col-4  m-2">
-                            
+
                                 <div className="card-body">
 
                                     <h5 className="card-id">Animal id</h5>
@@ -73,6 +88,7 @@ export default function ProductDetails(props : any) {
                                     <button type="button" className="btn btn-success">Success</button>
                                 </div>
                             </div>
+
                         </div>
 
                     </div>
@@ -81,3 +97,8 @@ export default function ProductDetails(props : any) {
         </div>
     )
 }
+const mapStateToProps = (state: any) => {
+    console.log(state);
+    return { state }
+}
+export default connect(mapStateToProps)(ProductDetails)
