d384dbe9b16768043539e609b9d23d89a2032408
Yash1th <yashwanthmukka2000@gmail.com>
2022-03-15 04:20:55 +0530

    get selected product data

--- locationPoductDetails/location_router.js
+++ locationPoductDetails/location_router.js
@@ -1,6 +1,8 @@
 
 const express = require('express');
 const expressAsyncHandler = require("express-async-handler");
+const { param } = require('express/lib/request');
+const { ObjectId } = require('mongodb');
 const router = express.Router();
 const Location = require('./marketingDetailsSchema.js');
 
@@ -16,7 +18,7 @@ router.post("/marketDetails", async (req, res) => {
         const myLocation = new Location(locationdb);
         await Location.create(myLocation);
         res.send(myLocation);
-        res.send({status:"success"})
+        res.send({ status: "success" })
     }
     catch (err) {
         res.send({ message: err })
@@ -71,6 +73,32 @@ router.get('/locations', async (req, res) => {
         res.send({ message: err })
     }
 })
+
+// fetching selected product details
+
+router.get('/selectedProductDetails/:id', async (req, res) => {
+    let { params } = req
+    let totalMarketsData = [];
+    try {
+        Location.find().exec(function (err, result) {
+            result.map(function (e) {
+                e.cattleMarkets.map(item => {
+                    totalMarketsData.push(item)
+                })
+                e.sheepMarkets.map(item => {
+                    totalMarketsData.push(item)
+                })
+            })
+            const productData = totalMarketsData.filter(e => e.animalId === params.id);
+            res.send(productData)
+        })
+    }
+    catch (err) {
+        res.send({ message: err })
+    }
+})
+
+
 ////////////////// Storing product details-----Team-5///////////////////
 let cattleMarketData = {};
 let sheepMarketData = {};
@@ -81,7 +109,7 @@ router.post("/updateDetails", async (req, res) => {
         console.log('productData', prodData);
         cattleMarketData =
         {
-            animalId:"222",
+            animalId: "222",
             productCode: "c23ds",
             availability: prodData.availability,
             image: prodData.image,
@@ -93,8 +121,8 @@ router.post("/updateDetails", async (req, res) => {
             certification: prodData.certification,
         }
         sheepMarketData = {
-            
-            animalId:"200",
+
+            animalId: "200",
             productCode: "c23ds",
             availability: prodData.availability,
             image: prodData.image,
@@ -104,28 +132,28 @@ router.post("/updateDetails", async (req, res) => {
             source: prodData.source,
             certification: prodData.certification,
         }
-        Location.findOne({locationName: prodData.location}).exec(function (err, result) {
+        Location.findOne({ locationName: prodData.location }).exec(function (err, result) {
             // console.log("before adding products",result);
-            console.log("err",err);
-            let cattleMarkets= result.cattleMarkets;
+            console.log("err", err);
+            let cattleMarkets = result.cattleMarkets;
             let sheepMarkets = result.sheepMarkets;
-            if(prodData.market=='Cattle Market'){
+            if (prodData.market == 'Cattle Market') {
                 result.cattleMarkets.push(cattleMarketData)
-                Location.updateOne({locationName:prodData.location},
+                Location.updateOne({ locationName: prodData.location },
                     result
-                    ).exec(function(err,result){
-                       return res.send({status:"success",result})
-                    })
-            }else if(prodData.market=='Sheep Market'){
+                ).exec(function (err, result) {
+                    return res.send({ status: "success", result })
+                })
+            } else if (prodData.market == 'Sheep Market') {
                 result.sheepMarkets.push(sheepMarketData)
-                Location.updateOne({locationName:prodData.location},
+                Location.updateOne({ locationName: prodData.location },
                     result
-                    ).exec(function(err,result){
-                       return res.send({status:"success",result})
-                    })
+                ).exec(function (err, result) {
+                    return res.send({ status: "success", result })
+                })
             }
             // console.log("after adding products",result);
-         // else
+            // else
             // res.send({status:"success"})
         })
     }
@@ -140,46 +168,46 @@ router.post("/updateDetails", async (req, res) => {
 router.get(
     "/get-animal/:locationName/:animalId",
     expressAsyncHandler(async (req, res) => {
-      console.log(res);
-      let locationName = req.params.locationName;
-      let animalId = req.params.animalId;
-      //   console.log(animalId);
-      let locationdbs = await Location.findOne({ locationName: locationName });
-       console.log(locationdbs);
-      if (locationdbs == null) {
-        res
-          .status(200)
-          .send({ message: "location does not exist with this location name" });
-      } else {
-        
-        let animalData = locationdbs.cattleMarkets.find(
-          (animal) => animal.animalId == animalId
-        );
-        if (animalData == null) {
-          res.status(200).send({
-            message: "Animal data doesn't exist with this animal Id!!",
-          });
+        console.log(res);
+        let locationName = req.params.locationName;
+        let animalId = req.params.animalId;
+        //   console.log(animalId);
+        let locationdbs = await Location.findOne({ locationName: locationName });
+        console.log(locationdbs);
+        if (locationdbs == null) {
+            res
+                .status(200)
+                .send({ message: "location does not exist with this location name" });
         } else {
-          data = {
-            price: animalData.price,
-            animalId: animalData.animalId,
-            weight: animalData.weight,
-            image:animalData.image,
-            breed:animalData.breed,
-            availability:animalData.availability,
-            certification:animalData.certification,
-            source:animalId.source
-          };
-          res.status(200).send({
-             message: "Animal data fetched successfully!!",
-            payload: data,
-          });
-          console.log(animalData);
+
+            let animalData = locationdbs.cattleMarkets.find(
+                (animal) => animal.animalId == animalId
+            );
+            if (animalData == null) {
+                res.status(200).send({
+                    message: "Animal data doesn't exist with this animal Id!!",
+                });
+            } else {
+                data = {
+                    price: animalData.price,
+                    animalId: animalData.animalId,
+                    weight: animalData.weight,
+                    image: animalData.image,
+                    breed: animalData.breed,
+                    availability: animalData.availability,
+                    certification: animalData.certification,
+                    source: animalId.source
+                };
+                res.status(200).send({
+                    message: "Animal data fetched successfully!!",
+                    payload: data,
+                });
+                console.log(animalData);
+            }
         }
-      }
     })
-  );
-  
+);
+
 
 
 module.exports = router
--- locationPoductDetails/marketingDetailsSchema.js
+++ locationPoductDetails/marketingDetailsSchema.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 
 const cattleMarketSchema = new mongoose.Schema({
-  animalId: { type: String, required: [true, "Id is required"] },
+  animalId: { type: String, required: [true, "Id is required"], unique: true },
   productCode: { type: String, required: true },
   availability: {
     type: String,
     required: true,
     enum: ["In Stock", "Out of Stock"],
   },
-  image:{type:Buffer,required:true},
+  image: { type: Buffer, required: true },
   price: { type: String, required: true },
   weight: { type: String, required: true },
   breed: { type: String, required: true },
@@ -21,14 +21,14 @@ const cattleMarketSchema = new mongoose.Schema({
   },
 });
 const sheepMarketSchema = new mongoose.Schema({
-  animalId: { type: String, required: true },
+  animalId: { type: String, required: true, unique: true },
   productCode: { type: String, required: true },
   availability: {
     type: String,
     required: true,
     enum: ["In Stock", "Out of Stock"],
   },
-  image:{type:Buffer,required:true},
+  image: { type: Buffer, required: true },
   price: { type: String, required: true },
   weight: { type: String, required: true },
   breed: { type: String, required: true },
@@ -42,8 +42,9 @@ const sheepMarketSchema = new mongoose.Schema({
 const locationSchema = new mongoose.Schema(
   {
     locationName: {
-      type: String, required: true, minlength: 3, maxlength: 20, unique:true},
- 
+      type: String, required: true, minlength: 3, maxlength: 20, unique: true
+    },
+
     cattleMarkets: [cattleMarketSchema],
     sheepMarkets: [sheepMarketSchema],
   },
@@ -51,8 +52,8 @@ const locationSchema = new mongoose.Schema(
 );
 locationSchema.pre('save', function (next) {
   // capitalize
-  this.locationName=
-  this.locationName.trim()[0].toUpperCase() + this.locationName.slice(1).toLowerCase();
+  this.locationName =
+    this.locationName.trim()[0].toUpperCase() + this.locationName.slice(1).toLowerCase();
   next();
 })
 const Location = mongoose.model("locationDB", locationSchema);
