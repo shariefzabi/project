28ec3eb4936b4370bc25a29e70cb4531c6e65e6e
priankabaptist <95601571+priankabaptist@users.noreply.github.com>
2022-03-14 19:30:05 +0530

    modifications

--- src/state/app_store.jsx
+++ src/state/app_store.jsx
@@ -1,14 +1,14 @@
 // import { createStore } from "redux";
 import { createStore } from "redux";
-import appReducer from "./appReducers"
-const appStore = createStore(appReducer);
-export default appStore;
-
-
-
-
-// import shopReducer from '../../src/modules/shopping_cart/newcomponet/redux/Shopping/shopping-reducer';
-
-// const appStore = createStore(shopReducer);
+// import appReducer from "./appReducers"
+// const appStore = createStore(appReducer);
 // export default appStore;
+
+
+
+
+import shopReducer from '../../src/modules/shopping_cart/newcomponet/redux/Shopping/shopping-reducer';
+
+const appStore = createStore(shopReducer);
+export default appStore;
 
