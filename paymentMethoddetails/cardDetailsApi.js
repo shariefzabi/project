a82d831ae7d5aabbdad31dda2e90ef335c69bd72
zabiullah sharief mohammad <zabiullahshariefmohammed@gmail.com>
2022-03-14 21:53:34 +0530

    invoivce

--- paymentMethoddetails/cardDetailsApi.js
+++ paymentMethoddetails/cardDetailsApi.js
@@ -3,41 +3,41 @@ const router = express.Router();
 router.use(express.json())
 const Invoice = require("./carddetails.cjs");
 
-router.get('/cost/:id', function (req, res) {
-    let { params } = req;
+// router.get('/cost/:id', function (req, res) {
+//     let { params } = req;
 
-    const locationdetails = req.app.get('locationCollection')
-    // locationdetails.save();
-    locationdetails.findOne({}, function (err, result) {
-        if (err) {
-            console.log(err);
-            res.status(502).json(err)
-        }
-        else {
-            if ("cattleMarkets" in result) {
-                console.log("product details are available");
-                let price = result.cattleMarkets[params.id].price;
-                console.log("price", price)
-                res.json(price)
-            }
-            else {
-                console.log("product details are not available");
-                res.json(null)
-            }
-        }
-    })
-})
+//     const locationdetails = req.app.get('locationCollection')
+//     // locationdetails.save();
+//     locationdetails.findOne({}, function (err, result) {
+//         if (err) {
+//             console.log(err);
+//             res.status(502).json(err)
+//         }
+//         else {
+//             if ("cattleMarkets" in result) {
+//                 console.log("product details are available");
+//                 let price = result.cattleMarkets[params.id].price;
+//                 console.log("price", price)
+//                 res.json(price)
+//             }
+//             else {
+//                 console.log("product details are not available");
+//                 res.json(null)
+//             }
+//         }
+//     })
+// })
 
-router.get("/details/:id", async (req, res) => {
-    try {
-        let { params } = req;
-        console.log(params.id);
-        const locations = await Card.findOne({ "cardDetails.CVV": params.id });
-        res.json(locations);
-    } catch (err) {
-        res.send("Error" + err);
-    }
-});
+// router.get("/details/:id", async (req, res) => {
+//     try {
+//         let { params } = req;
+//         console.log(params.id);
+//         const locations = await Card.findOne({ "cardDetails.CVV": params.id });
+//         res.json(locations);
+//     } catch (err) {
+//         res.send("Error" + err);
+//     }
+// });
 
 
 
