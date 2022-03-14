6fb02a1f9a2183e6c8a147b6e94543e81a811347
yallamaraju-charan-varma <95687280+yallamaraju-charan-varma@users.noreply.github.com>
2022-03-14 11:37:34 +0530

    Update schema.cjs

--- Blogs/schema.cjs
+++ Blogs/schema.cjs
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 const Blogs = new mongoose.Schema({
 
-
-
-    blogs: {
         title: { type: String,required:true},
         about: { type: String,required:true},
         id:{type:Number,required:true},
         date: { type: Date, default:Date.now},
-    }
 }
 
 
 );
 module.exports = mongoose.model("blogs",Blogs);
+
+
+
+
+// case 'setCurrentBlog':
+//     return {...state, currentBlog: action.payload};
