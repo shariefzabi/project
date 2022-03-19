80d8b0609189e4768a76a49d645f423388a1d849
manjusunkad8899 <manjusunkad8899@gmail.com>
2022-03-19 14:15:20 +0530

    PM

--- src/modules/ordercreation/paymentMethod/paymentMethod.tsx
+++ src/modules/ordercreation/paymentMethod/paymentMethod.tsx
@@ -14,14 +14,14 @@ class PaymentMethod extends React.Component<any, any> {
       month_year: '',
       month: "",
       year: "",
-      
+
       orderId: 0,
-      date:"",
+      date: "",
       // err
       card_numberErr: '',
       cvv_numberErr: '',
       month_yearErr: '',
-    totalcost: "",
+      totalcost: "",
       cost: "",
       flag1: true,
       flag2: true,
@@ -30,10 +30,10 @@ class PaymentMethod extends React.Component<any, any> {
     }
   }
 
-  
-     
 
-  
+
+
+
   changeHandler = (event: any) => {
     // let { payment } = this.state;
     this.setState({ [event.target.name]: event.target.value });
@@ -124,7 +124,7 @@ class PaymentMethod extends React.Component<any, any> {
       })
     // alert()
     const date = new Date().toJSON().slice(0, 10).split('-').reverse().join('/')
-    this.setState({date})
+    this.setState({ date })
     let { payment, orderId } = this.state
     this.setState({ flag1: false })
     let paymentStatus = {
@@ -145,7 +145,7 @@ class PaymentMethod extends React.Component<any, any> {
     }
   }
   submitHandler = (e: any) => {
-    let tokens:any = []
+    let tokens: any = []
     e.preventDefault()
     this.setState({ flag2: false })
     let { month, year, payment, card_number, cvv_number, orderId, } = this.state;
@@ -153,15 +153,15 @@ class PaymentMethod extends React.Component<any, any> {
     let token = Math.round(Math.random() * 1000000000000);
     while (tokens.includes(token)) {
       token = Math.round(Math.random() * 1000000000000);
-     }
+    }
     tokens.push(token);
-    
-    
-    
+
+
+
     let paymentStatus = {
       date: this.state.date,
       paymentStatus: "payment success",
-      invoiceId :token
+      invoiceId: token
     }
     console.log(orderId)
     axios.post("http://localhost:3005/paymentstatus", paymentStatus)
@@ -172,7 +172,7 @@ class PaymentMethod extends React.Component<any, any> {
         console.log("error: ", err);
       })
     console.log("carddetails", { month, year, card_number, cvv_number })
-  
+
     this.props.setCardDetails({ month, year, card_number, cvv_number })
     console.log("originalInvoice", this.state.orderId)
     let cardDetails = {
@@ -208,9 +208,9 @@ class PaymentMethod extends React.Component<any, any> {
       cvv_number: '',
       month_year: ''
     })
-    
 
-   
+
+
 
 
   }
@@ -223,7 +223,7 @@ class PaymentMethod extends React.Component<any, any> {
       <div className='paymentmethod'>
         {/* <div className="modal fade" id="exampleModalPayment" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" data-bs-toggle="modal" tabIndex={-1} data-bs-backdrop="static"> */}
         {this.state.flag1 &&
-          <div className="modal-dialog modal-dialog-centered ">
+          <div className="modal-dialog  modal-md modal-dialog-centered m-0">
             <div className="modal-content">
               <div className="modal-body ">
                 <h5 className="modal-title " id="exampleModalToggleLabel">Payment Method</h5>
@@ -247,56 +247,54 @@ class PaymentMethod extends React.Component<any, any> {
         }
 
         {!this.state.flag1 &&
-          <div>
+          <div >
             {this.state.payment == "Ravepay" &&
-              <div>
-
-                <div className="modal-dialog modal-sm modal-dialog-centered">
-                  {this.state.flag2 &&
-                    <div className="modal-content">
-                      <div className="text-center mt-3 border-bottom border-2">
-                        <p className="form-text  text-center" id="exampleModalToggleLabel">Card</p>
-                        {/* <p className="form-text pe-4 me-4 text-center" id="exampleModalToggleLabel">Account</p> */}
-                        {/* <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
-                      </div>
-
-                      <div className="modal-body ">
-                        <p className="form-text ps-1 text-start m-0">Fill in your data</p>
-
-                        <form  >
-                          {/* card Number */}
-                          <img className="placeholder-img" src={require("./asset/img/card.png")} alt=""></img>
-                          <input className=" form-control pt-2 pb-2" type="number" value={this.state.card_number} name="card_number" placeholder="Card number" onChange={this.changeHandler} onBlur={this.validation} />
-                          <p className="text-danger text-start m-0">{this.state.card_numberErr}</p>
-                          {/* Month year */}
-                          <img className="placeholder-img" src={require("./asset/img/calendar.png")} alt=""></img>
-                          <input className="form-control pt-2 pb-2 " type="text" value={this.state.month_year} placeholder="MM/YY" name="month_year" onChange={this.changeHandler} onBlur={this.validation} />
-                          <p className="text-danger text-start m-0">{this.state.month_yearErr}</p>
-                          {/* CVV number */}
-                          <img className="placeholder-img" src={require("./asset/img/CVV.png")} alt=""></img>
-                          <input className="form-control  pt-2 pb-2" type="number" value={this.state.cvv_number} name="cvv_number" placeholder="CVV" onChange={this.changeHandler} onBlur={this.validation} />
-                          <p className="text-danger text-start m-0">{this.state.cvv_numberErr}</p>
-                          <div className="text-center mb-3 mt-4 ">
-                            <button onClick={(e) => { this.submitHandler(e) }} className="btn button-large btn-success mt-3" disabled={!(this.state.card_numberErr == '' && this.state.cvv_numberErr == '' && this.state.card_number !== '' && this.state.cvv_number !== '')}><img className="lock-icon" src={require("./asset/img/lock.png")}></img>Pay &#8377;  {` ${this.state.totalcost}.00`}</button>
-                            {/* <button onClick={(e) => this.submitHandler(e)} className="btn button-large btn-success mt-3" data-bs-target="#exampleModalToggle3" data-bs-toggle="modal" ><img className="lock-icon" src={require("./asset/img/lock.png")}></img>Pay #200.00</button> */}
-                          </div>
-                        </form>
-                      </div>
+              <div className="modal-dialog modal-sm modal-dialog-centered mt-0">
+                {this.state.flag2 &&
+                  <div className="modal-content">
+                    <div className="text-center mt-3 border-bottom border-2">
+                      <p className="form-text  text-center" id="exampleModalToggleLabel">Card</p>
+                      {/* <p className="form-text pe-4 me-4 text-center" id="exampleModalToggleLabel">Account</p> */}
+                      {/* <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button> */}
                     </div>
-                  }
-                  {!this.state.flag2 &&
-                    <div className="modal-content">
-                      <div className="modal-body text-center">
-                        <div className="Successfully_Placed_box">
-                          <h3>&#10003;</h3>
+
+                    <div className="modal-body ">
+                      <p className="form-text ps-1 text-start m-0">Fill in your data</p>
+
+                      <form  >
+                        {/* card Number */}
+                        <img className="placeholder-img" src={require("./asset/img/card.png")} alt=""></img>
+                        <input className=" form-control pt-2 pb-2" type="number" value={this.state.card_number} name="card_number" placeholder="Card number" onChange={this.changeHandler} onBlur={this.validation} />
+                        <p className="text-danger text-start m-0">{this.state.card_numberErr}</p>
+                        {/* Month year */}
+                        <img className="placeholder-img" src={require("./asset/img/calendar.png")} alt=""></img>
+                        <input className="form-control pt-2 pb-2 " type="text" value={this.state.month_year} placeholder="MM/YY" name="month_year" onChange={this.changeHandler} onBlur={this.validation} />
+                        <p className="text-danger text-start m-0">{this.state.month_yearErr}</p>
+                        {/* CVV number */}
+                        <img className="placeholder-img" src={require("./asset/img/CVV.png")} alt=""></img>
+                        <input className="form-control  pt-2 pb-2" type="number" value={this.state.cvv_number} name="cvv_number" placeholder="CVV" onChange={this.changeHandler} onBlur={this.validation} />
+                        <p className="text-danger text-start m-0">{this.state.cvv_numberErr}</p>
+                        <div className="text-center mb-3 mt-4 ">
+                          <button onClick={(e) => { this.submitHandler(e) }} className="btn button-large btn-success mt-3" disabled={!(this.state.card_numberErr == '' && this.state.cvv_numberErr == '' && this.state.card_number !== '' && this.state.cvv_number !== '')}><img className="lock-icon" src={require("./asset/img/lock.png")}></img>Pay &#8377;  {` ${this.state.totalcost}.00`}</button>
+                          {/* <button onClick={(e) => this.submitHandler(e)} className="btn button-large btn-success mt-3" data-bs-target="#exampleModalToggle3" data-bs-toggle="modal" ><img className="lock-icon" src={require("./asset/img/lock.png")}></img>Pay #200.00</button> */}
                         </div>
-                        <h5 className='Successfully_Placed'>Payment succesful</h5>
-                        {/* <button className="btn btn-success" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Continue Shopping</button> */}
-                      </div>
+                      </form>
                     </div>
-                  }
-                </div>
+                  </div>
+                }
+                {!this.state.flag2 &&
+                  <div className="modal-content">
+                    <div className="modal-body text-center">
+                      <div className="Successfully_Placed_box">
+                        <h3>&#10003;</h3>
+                      </div>
+                      <h5 className='Successfully_Placed'>Payment succesful</h5>
+                      {/* <button className="btn btn-success" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Continue Shopping</button> */}
+                    </div>
+                  </div>
+                }
               </div>
+
             }
             {this.state.payment == "Pay_On_Delivery" &&
               //  <div className="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabIndex={-1}>
