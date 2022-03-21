1669af701938fbf2b27c8470f4eaf15d05fbd5d3
zabiullah sharief mohammad <zabiullahshariefmohammed@gmail.com>
2022-03-21 12:52:44 +0530

    pd

--- paymentMethoddetails/cardDetailsApi.js
+++ paymentMethoddetails/cardDetailsApi.js
@@ -2,43 +2,7 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json())
 const CardData = require("./carddetails.cjs");
-// const paymentStatus = require("./paymentSchema.cjs");
 
-// router.get('/cost/:id', function (req, res) {
-//     let { params } = req;
-
-//     const locationdetails = req.app.get('locationCollection')
-//     // locationdetails.save();
-//     locationdetails.findOne({}, function (err, result) {
-//         if (err) {
-//             console.log(err);
-//             res.status(502).json(err)
-//         }
-//         else {
-//             if ("cattleMarkets" in result) {
-//                 console.log("product details are available");
-//                 let price = result.cattleMarkets[params.id].price;
-//                 console.log("price", price)
-//                 res.json(price)
-//             }
-//             else {
-//                 console.log("product details are not available");
-//                 res.json(null)
-//             }
-//         }
-//     })
-// })
-
-// router.get("/details/:id", async (req, res) => {
-//     try {
-//         let { params } = req;
-//         console.log(params.id);
-//         const locations = await Card.findOne({ "cardDetails.CVV": params.id });
-//         res.json(locations);
-//     } catch (err) {
-//         res.send("Error" + err);
-//     }
-// });
 
 
 
@@ -69,21 +33,7 @@ router.post("/details", async (req, res) => {
     }
 });
 
-// router.post("/status", async (req, res) => {
-    
-//     try {
-    
-//         const myPaymentStatus = new paymentStatus(req.body);
-//         await paymentStatus.create(myPaymentStatus);
-//         res.send(myPaymentStatus);
-//         console.log(myPaymentStatus)
-//     } catch (err) {
-//         res.send({ message: err });
-//     }
-    
 
-    
-// })
     
 
 
--- paymentMethoddetails/carddetails.cjs
+++ paymentMethoddetails/carddetails.cjs
@@ -17,31 +17,3 @@ const CardData = new mongoose.Schema({
 module.exports = mongoose.model("carddatas", CardData);
 
 
-// const mongoose = require("mongoose");
-// const Card = new mongoose.Schema({
-//     cardDetails: {
-//         cardNumber: { type: Number, required: true, min: 100000000000, max: 999999999999, },
-//         Month: { type: Number, required: true, min: 1, max: 12 },
-//         year: { type: Number, required: true, min: 22 },
-//         CVV: { type: Number, required: true, max: 000, max: 999 },
-//     }
-// }
-// );
-// module.exports = mongoose.model("cardDetails", Card);
-
-// { "invoiceDetails": {
-//     "orderId": 12345677,
-//     "cardNumber": 123456789012,
-//     "Month":12,
-//     "year": 24,
-//     "CVV": 234,
-//     "type": "cow",
-//     "quantity": "2",
-//     "sex": "Male",
-//     "weight": "5Kg",
-//     "breed": "breed",
-//     "deliverylocation": "location",
-//     "deliveryperiod": "1-7",
-//     "deliverymode": "offline"
-//     }
-// }
