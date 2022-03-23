6f8f0b39fe114e5c3ced09aa4bbd0b318e6dba9a
zabiullah sharief mohammad <zabiullahshariefmohammed@gmail.com>
2022-03-23 14:13:34 +0530

    Update invoiceapi.js

--- invoice/invoiceapi.js
+++ invoice/invoiceapi.js
@@ -2,14 +2,21 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json())
 router.get("/details", async (req, res) => {
-    let { useremail } = req.query;
-    // console.log(useremail);
-    const ordercollection = req.app.get('ordercollection')
+    try {
+        let { useremail } = req.query;
+    
+        const ordercollection = req.app.get('ordercollection')
 
-    // let ordercollection = db.collection("orders");
-    ordercollection.find({ paymentStatus: "payment success", email: useremail }).sort({ "_id": -1 }).toArray(function (err, result) {
-        res.send(result)
-    })
+    
+        ordercollection.find({ paymentStatus: "payment success", email: useremail }).sort({ "_id": -1 }).toArray(function (err, result) {
+            res.send(result)
+        
+            
+        })
+    }
+    catch (err) {
+        res.status(400).send("error while fteching invoice data ")
+    }
 });
 
 
